abf3d1902e65bf8558d7e5d2c6bf255af1dcf41d
maohui <501206021@qq.com>
2018-03-08 19:41:20 +0800

    + leetcode_001

---
+++ src/leetcode_001/Solution.java
@@ -0,0 +1,30 @@
+package leetcode_001;
+
+/**
+ * 给定一个整数数列，找出其中和为特定值的那两个数。
+ * <p>
+ * 你可以假设每个输入都只会有一种答案，同样的元素不能被重用。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+
+        }
+        return null;
+    }
+}
